dd5105612ea9cab4a130a120d7f0758ec22afba3
JDH0123 <jjang8339458@gmail.com>
2024-09-06 13:09:25 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+
+//2475
+
+// int main()
+// {
+//     int a, b, c, d, e;
+//     scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+//     printf("%d", (a*a + b*b + c*c + d*d + e*e) % 10);
+//     return 0;
+// }
+
+//31403
+// int main()
+// {
+//     int a, b ,c;
+//     scanf("%d", &a);
+//     scanf("%d", &b);
+//     scanf("%d", &c);
+
+//     printf("%d\n", a+b-c);
+//     printf("%d", tmp - c);
+// }
+
+//10250
+// int main()
+// {
+//     int a, b, c, d, n;
+//     scanf("%d", &n);
+
+//     while (n > 0)
+//     {
+//         scanf("%d %d %d", &a, &b, &c);
+        
+//         d = 1;
+
+//         while (c > a)
+//         {
+//             c -= a ;
+//             d += 1 ;
+//         }
+
+//         if (d < 10) 
+//         {
+//             printf("%d%d%d\n", c,0,d);
+//         }
+
+//         else 
+//         {
+//             printf("%d%d\n", c,d);
+//         }
+        
+//         n -= 1 ;
+//     }
+// }
+
+//2577
+// int main()
+// {
+//     int a, b, c, tmp;
+//     scanf("%d\n", &a);
+//     scanf("%d\n", &b);
+//     scanf("%d", &c);
+    
+//     tmp = a*b*c;
+//     int count[10] = {0}; 
+
+//     while (tmp > 0) 
+//     {
+//         count[tmp % 10] += 1;
+//         tmp = tmp / 10;
+//     }
+
+//     for (int i = 0; i < 10; i++)
+//     {
+//         printf("%d\n", count[i]);
+//     }
+// }
+
+//2920
+int main()
+{
+    int a, b, c, d, e, f, g, h;
+    scanf("%d %d %d %d %d %d %d %d",&a, &b, &c, &d, &e, &f ,&g, &h);
+    int res[8] = {a, b, c, d, e, f, g, h}; 
+
+    int asc = 1;
+    for (int i = 0; i < 8; i++)
+    {
+        if (res[i] != i+1)
+        {
+            asc = 0;
+            break;
+        }
+    }
+    
+    int des = 1;
+    for (int i = 0; i < 8; i++)
+    {
+        if (res[i] != 8-i)
+        {
+            des = 0;
+            break;
+        }
+    }
+
+    if (asc)
+    {
+        printf("ascending");
+    }
+
+    else if (des)
+    {
+        printf("descending");
+    }
+
+    else 
+    {
+        printf("mixed");
+    }
+}
