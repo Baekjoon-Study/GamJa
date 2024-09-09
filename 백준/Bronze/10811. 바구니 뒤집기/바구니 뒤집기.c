a5363865843707103cab25080dabf3932936e23a
JDH0123 <jjang8339458@gmail.com>
2024-09-09 19:58:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
@@ -2,23 +2,23 @@
 
 int main()
 {
-    int n, m, a, b, tmp;
+    int n,m,a,b,tmp;
     scanf("%d %d", &n, &m);
-
+    
     int arr[n];
     for (int i = 0; i < n; i++) arr[i] = i + 1;
-
-    for (int i = 0; i < m; i++)
+    
+    for (int i = 0; i < m; i++) 
     {
         scanf("%d %d", &a, &b);
-        for (int j = 0; j < (b-a+1)/2; j++)
+        for (int j = 0; j < (b-a+1)/2; j++) 
         {
             tmp = arr[a+j-1];
             arr[a+j-1] = arr[b-j-1];
             arr[b-j-1] = tmp;
         }
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) 
     {
         printf("%d ", arr[i]);
     }
