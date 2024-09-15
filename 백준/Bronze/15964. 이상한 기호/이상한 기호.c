b33930ae646adf464f47998da27aaf17c0ef8b63
JDH0123 <jjang8339458@gmail.com>
2024-09-15 14:22:34 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.c
@@ -0,0 +1,70 @@
+#include <stdio.h> 
+
+//2738
+// int main(void)
+// {
+//     int n, m;
+//     scanf("%d %d", &n, &m);
+
+//     int arr1[n][m];
+//     int arr2[n][m];
+
+//     int s;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             scanf("%d", &arr1[i][j]);
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             scanf("%d", &arr2[i][j]);
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < m; j++)
+//         {
+//             printf("%d ", arr1[i][j]+ arr2[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+// }
+
+//10872
+
+// int main(void)
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     if (n == 0)
+//     {
+//         n = 1;
+//     }
+
+//     int s = 1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         s *= i;
+//     }
+
+//     printf("%d", s);
+// }
+
+//15964
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", (a+b)*(a-b));
+}
