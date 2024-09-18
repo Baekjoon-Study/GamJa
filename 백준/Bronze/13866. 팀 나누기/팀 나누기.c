412b9c5f89726e648d9f6c83a7a5d4ca8124ba7c
JDH0123 <jjang8339458@gmail.com>
2024-09-18 17:02:20 +0900

    [Bronze IV] Title: 팀 나누기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+//15740
+
+// int main(void)
+// {
+//     double a, b;
+//     scanf("%lf %lf", &a, &b);
+//     printf("%lld", (long long int)(a+b));
+// }
+
+//27323
+
+// int main(void)
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     printf("%d", a*b);
+// }
+
+//4999
+// #include <string.h>
+
+// int main(void)
+// {
+//     char  a[1000], b[1000];
+//     scanf("%s", &a);
+//     scanf("%s", &b);
+//     if (strlen(a) >= strlen(b))
+//     {
+//         printf("go");
+//     }
+//     else 
+//     {
+//         printf("no");
+//     }
+// }
+
+//30007
+
+// int main(void) 
+// {
+//     int N;
+//     scanf("%d", &N);
+
+//     int a, b, x;
+
+//     for (int i = 0; i < N; i++)
+//     {
+//         scanf("%d %d %d", &a, &b, &x);
+//         printf("%d\n", a*(x-1)+b);
+//     }
+// }
+
+//13866
+
+int main(void)
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int tmp;
+    if (a > b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    if (b > c)
+    {
+        tmp = b;
+        b = c;
+        c = tmp;
+    }
+    if (c > d)
+    {
+        tmp = c;
+        c = d;
+        d = tmp;
+    }
+    if (a > b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    if (b > c)
+    {
+        tmp = b;
+        b = c;
+        c = tmp;
+    }
+    if (a > b)
+    {
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    int m = (a+d)-(b+c);
+
+    if (m >= 0)
+    {
+        printf("%d", m);
+    }
+    else if (m < 0)
+    {
+        printf("%d", -m);
+    }
+}
